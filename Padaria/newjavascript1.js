db8b0e49e5ba8dd55df4aad4808ef062eaf57e30
VitorHBuss <125294270+VitorHBuss@users.noreply.github.com>
2023-09-06 21:19:33 -0300

    Delete Padaria directory

--- Padaria/newjavascript1.js
+++
@@ -1,6 +0,0 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-const elemento = document. getElementById("comprafinal");
-window.alert("Compra finalizada com sucesso!");
